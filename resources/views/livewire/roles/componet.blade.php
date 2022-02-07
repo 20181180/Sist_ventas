46f6fd4bd6a4adf2b8de179f53bf379261b6bb73
20181180 <20181180@uthh.edu.mx>
2022-02-07 17:20:52 -0600

    roles_controller

--- resources/views/livewire/roles/componet.blade.php
+++ resources/views/livewire/roles/componet.blade.php
@@ -3,7 +3,7 @@
     <div class="col-sm-12 ">
         <div class="widget widget-chart-one">
             <div class="widget-heading">
-                <h4 class="card-title"><b>{{$componentName}} | {{$pageTitle}}</b></h4>
+                <h4 class="card-title"><b>{{ $componentName }} | {{ $pageTitle }}</b></h4>
                 <ul class="tabs tab-pills">
                     <li><a href="javascript:void(0);" class="tabmenu bg-dark" data-toggle="modal" data-target="#theModal">Agregar</a></li>
                 </ul>
@@ -15,59 +15,78 @@
                     <table class="table table-bordered table-striped  mt-1">
                         <thead class="text-white" style="background: #3B3F5C">
                             <tr>
-                                <th class="table-th text-white">DESCRIPCIÓN</th>
-                                <th class="table-th text-center text-white">IMAGEN</th>
+                                <th class="table-th text-white">CLAVE</th>
+                                <th class="table-th text-center text-white">DESCRIPCIÓN</th>
                                 <th class="table-th text-center text-white">ACTIONS</th>
                             </tr>
                         </thead>
                         <tbody>
-
-                            @foreach($categories as $category)
+                            @foreach($roles as $role)
                             <tr>
                                 <td>
-                                    <h6>{{$category->name}}</h6>
+                                    <h6>{{$role->id}}</h6>
                                 </td>
 
                                 <td class="text-center">
-                                    <span>
-                                        <img src="{{ asset('storage/' .$category->imagen) }}" alt="ejemplo" height="70" width="80" class="rounded">
-                                    </span>
+                                    <h6>{{$role->name}}</h6>
                                 </td>
 
                                 <td class="text-center">
-                                    <a href="javascript:void(0);" wire:click="Edit({{$category->id}})" class="btn btn-dark mtmobile" title="Edit">
+                                    <a href="javascript:void(0);" wire:click="Edit({{$role->id}})" class="btn btn-dark mtmobile" title="Editar registro">
                                         <i class="fas fa-edit"></i>
                                     </a>
 
-                                    <a href="javascript:void(0);" onclick="Confirm('{{$category->id}}','{{$category->products->count()}}')" class="btn btn-dark" title="Delete">
+                                    <a href="javascript:void(0);" onclick="Confirm('{{$role->id}}')" class="btn btn-dark" title="Eliminar registro">
                                         <i class="fas fa-trash"></i>
                                     </a>
 
                                 </td>
+
                             </tr>
+
                             @endforeach
                         </tbody>
                     </table>
-                    {{$categories->links()}}
+                    {{$roles->links()}}
                 </div>
             </div>
         </div>
     </div>
-    @include('livewire.category.form')
+    @include('livewire.roles.form')
 </div>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        window.livewire.on('show-modal', msg => {
-            $('#theModal').modal('show');
+        window.livewire.on('role-added', Msg => {
+            $('#theModal').modal('hide')
+            noty(Msg)
+
         });
-        window.livewire.on('category-added', msg => {
-            $('#theModal').modal('hide');
+        window.livewire.on('role-updated', Msg => {
+            $('#theModal').modal('hide')
+            noty(Msg)
         });
-        window.livewire.on('category-updated', msg => {
-            $('#theModal').modal('hide');
+
+        window.livewire.on('role-deleted', Msg => {
+            noty(Msg)
         });
-    });
+
+        window.livewire.on('role-exists', Msg => {
+            noty(Msg)
+        });
+
+        window.livewire.on('role-error', Msg => {
+            noty(Msg)
+        });
+
+        window.livewire.on('hide-modal', Msg => {
+            $('#theModal').modal('hide')
+        });
+
+        window.livewire.on('show-modal', Msg => {
+            $('#theModal').modal('show')
+        });
+    })
 
     function Confirm(id, products) {
         if (products > 0) {
@@ -76,7 +95,7 @@
         }
         swal({
             title: 'CONFIRMAR',
-            text: '¿CONFIRMAS ELIMINAR EL REGISTRO?',
+            text: '¿CONFIRMAS ELIMINAR EL ROL?',
             type: 'warning',
             showCancelButton: true,
             cancelButtonText: 'Cerrar',
