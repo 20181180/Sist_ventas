3bef9fcbe00eac9a6886a0459e326dc7e2905c45
20181180 <20181180@uthh.edu.mx>
2022-02-16 11:58:37 -0600

    Auth

--- resources/views/layouts/theme/header.blade.php
+++ resources/views/layouts/theme/header.blade.php
@@ -7,7 +7,6 @@
                 </a>
             </li>
         </ul>
-
         <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list">
                 <line x1="8" y1="6" x2="21" y2="6"></line>
                 <line x1="8" y1="12" x2="21" y2="12"></line>
@@ -18,21 +17,24 @@
             </svg></a>
 
         <livewire:search-controller>
-
             <ul class="navbar-item flex-row navbar-dropdown">
-
-
                 <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img src="assets/img/icono.png" alt="admin-profile" class="img-fluid">
                     </a>
                     <div class="dropdown-menu position-absolute animated fadeInUp" aria-labelledby="userProfileDropdown">
+                        @guest
+                        @if (Route::has('login'))
+
+                        @endif
+                        @else
                         <div class="user-profile-section">
                             <div class="media mx-auto">
                                 <img src="assets/img/90x90.jpg" class="img-fluid mr-2" alt="avatar">
                                 <div class="media-body">
                                     <h5>{{ Auth::user()->name }}</h5>
                                     <p>Perfil: {{ Auth::user()->profile }}</p>
+
                                 </div>
                             </div>
                         </div>
@@ -43,8 +45,7 @@
                             </a>
                         </div>
                         <div class="dropdown-item">
-                            <a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit()
-                        ">
+                            <a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
                                     <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
@@ -56,6 +57,7 @@
                                 @csrf
                             </form>
                         </div>
+                        @endguest
                     </div>
                 </li>
             </ul>
