b1a6ab085075f2be0c0ee72728ef098a11e0a345
20181180 <20181180@uthh.edu.mx>
2022-04-13 13:22:54 -0500

    informes

--- app/Http/Livewire/InformesController.php
+++ app/Http/Livewire/InformesController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use App\Models\Cliente;
 use App\Models\Product;
 use Livewire\Component;
 use Barryvdh\DomPDF\Facade as PDF;
 
 class InformesController extends Component
 {
+    public $deu, $m, $prod_bj, $prod_exi,$catg_p, $estado;
+    public function mount()
+    {
+        $this->deu = [];
+        $this->estado = 0;
+        $this->prod_bj = [];
+        $this->prod_exi=[];
+    }
     public function render()
     {
         return view('livewire.informe.informes', [
             'products' => Product::join('categories as c', 'c.id', 'products.category_id')
                 ->select('products.*', 'c.name as category')
-                ->where('stock', '<', 1)
+
                 ->orderBy('products.stock', 'asc')->get(),
 
         ])->extends('layouts.theme.app')
@@ -26,7 +35,6 @@ class InformesController extends Component
     {
         $data = [];
         $data = Product::join('categories as c', 'c.id', 'products.category_id')
-
             ->select('products.*', 'c.name as category')
             ->where('stock', '<', 1)->get();
 
@@ -35,7 +43,38 @@ class InformesController extends Component
         $pdf = PDF::loadView('pdf.produc_baj', compact('data'));
         return $pdf->stream('salesReport.pdf');
         return $pdf->download('salesReport.pdf');
+    }
 
-        //Cart::clear(); //limpiamos e inicializamos las varibles..
+    public function client_deud()
+    {
+        // $this->deu = Cliente::where('estado', '>', 0)->get();
+        $this->deu = Cliente::join('meripuntos as m', 'm.client_id', 'clientes.id')
+            ->where('clientes.estado', 'activo')
+            ->where('saldo', '>', 0)->get();
+        $this->estado = 1;
+    }
+
+    public function meri()
+    {
+        // $this->deu = Cliente::where('estado', '>', 0)->get();
+        $this->deu = Cliente::join('meripuntos as m', 'm.client_id', 'clientes.id')
+            ->where('clientes.estado', 'activo')
+            ->where('meripuntos', '>', 0)->get();
+        $this->estado = 2;
+    }
+
+    public function pro_bajos()
+    {
+        $this->prod_bj = Product::join('categories as c', 'c.id', 'products.category_id')
+            ->select('products.*', 'c.name as category')
+            ->where('stock', '<', 1)->get();
+        $this->estado = 3;
+    }
+    public function pro_exis()
+    {
+        $this->prod_exi = Product::join('categories as c', 'c.id', 'products.category_id')
+            ->select('products.*', 'c.name as category')
+            ->where('stock', '>=', 1)->get();
+        $this->estado = 4;
     }
 }
--- resources/views/livewire/informe/informes.blade.php
+++ resources/views/livewire/informe/informes.blade.php
@@ -6,18 +6,69 @@
                 <h4 class="card-title"><b> MODULO DE INFORMES</b></h4>
             </div>
             <div class="widget-content">
-                <a class="btn btn-dark">Deudores</a>
-                <a class="btn btn-dark">Meripuntos</a>
-                <a class="btn btn-dark" href="{{ url('produc_baj/pdf')}}" target="_black">Productos Bajos</a>
-                <a class="btn btn-dark">Generar catalogo de Productos</a>
-                <a class="btn btn-dark">Catalogo de Productos</a>
+                <a class="btn btn-dark" wire:click="client_deud()">Deudores</a>
+                <a class="btn btn-dark" wire:click="meri()">Meripuntos</a>
+
+                <a class="btn btn-dark" wire:click="pro_bajos()">Productos Bajos</a>
+
+                <a class="btn btn-dark" wire:click="pro_exis()">Productos Existentes</a>
+
             </div>
             <br>
             <div class="widget-content">
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped  mt-1">
+                        @if($estado==0)
+
+                        <thead class="text-white" style="background: #226F">
+
+                            <tr>
+                                <a class="btn btn-dark btn-block {{count($products) <1 ? 'disabled' : '' }}" href=" {{ url('produc_baj/pdf')}}" target="_black">GENERAR PDF DE CATALOGO DE PRODUCTOS</a>
+                                <br>
+                                <th class="table-th text-white">NOMBRE</th>
+                                <th class="table-th text-white text-center">CODIGO BARRA</th>
+                                <th class="table-th text-white text-center">CATEGORIA</th>
+                                <th class="table-th text-white text-center">IMAGEN</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(count($products) <1 ) <tr>
+                                <td class="text-center" colspan="5">
+                                    <h5>No hay resultados.</h5>
+                                </td>
+                                </tr>
+                                @endif
+
+
+                                @foreach ($products as $product)
+                                <tr>
+                                    <td>
+                                        <h6>{{$product->name}}</h6>
+                                    </td>
+                                    <td>
+                                        <h6 class="text-center">{{$product->barcode}}</h6>
+                                    </td>
+                                    <td>
+                                        <h6 class="text-center">{{$product->category}}</h6>
+                                    </td>
+
+                                    <td class="text-center">
+                                        <span>
+                                            <img src=" {{asset('storage/' . $product->imagen )}}" onclick="ShowImg('{{ asset('storage/' . $product->imagen) }}','{{$product->name}}')" height="70" width="80" class="rounded zom" alt="no-image">
+                                        </span>
+                                    </td>
+
+                                </tr>
+
+                                @endforeach
+
+                        </tbody>
+                        @elseif($estado == 3)
                         <thead class="text-white" style="background: #226F">
                             <tr>
+                                <a class="btn btn-dark btn-block {{count($prod_bj) <1 ? 'disabled' : '' }}">GENERAR PDF DE PRODUCTOS BAJOS</a>
+                                <br>
                                 <th class="table-th text-white">NOMBRE</th>
                                 <th class="table-th text-white text-center">CODIGO BARRA</th>
 
@@ -29,33 +80,158 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($products as $product)
+                            @if(count($prod_bj) <1 ) <tr>
+                                <td class="text-center" colspan="5">
+                                    <h5>No hay resultados.</h5>
+                                </td>
+                                </tr>
+                                @endif
+
+                                @foreach ($prod_bj as $product)
+                                <tr>
+                                    <td>
+                                        <h6>{{$product->name}}</h6>
+                                    </td>
+                                    <td>
+                                        <h6 class="text-center">{{$product->barcode}}</h6>
+                                    </td>
+
+                                    <td>
+                                        <h6 class="text-center">{{$product->stock}}</h6>
+                                    </td>
+
+                                    <td>
+                                        <h6 class="text-center">{{$product->category}}</h6>
+                                    </td>
+
+                                    <td class="text-center">
+                                        <span>
+                                            <img src=" {{asset('storage/' . $product->imagen )}}" onclick="ShowImg('{{ asset('storage/' . $product->imagen) }}','{{$product->name}}')" height="70" width="80" class="rounded zom" alt="no-image">
+                                        </span>
+                                    </td>
+
+                                </tr>
+
+                                @endforeach
+                        </tbody>
+                        @elseif($estado == 1)
+                        <thead class="text-white" style="background: #3B3F5C">
                             <tr>
-                                <td>
-                                    <h6>{{$product->name}}</h6>
-                                </td>
-                                <td>
-                                    <h6 class="text-center">{{$product->barcode}}</h6>
-                                </td>
+                                <a class="btn btn-dark btn-block {{count($deu) <1 ? 'disabled' : '' }}" href="#" target="_black">GENERAR PDF DEUDORES</a>
 
-                                <td>
-                                    <h6 class="text-center">{{$product->stock}}</h6>
-                                </td>
-
-                                <td>
-                                    <h6 class="text-center">{{$product->category}}</h6>
-                                </td>
-
-                                <td class="text-center">
-                                    <span>
-                                        <img src=" {{asset('storage/' . $product->imagen )}}" onclick="ShowImg('{{ asset('storage/' . $product->imagen) }}','{{$product->name}}')" height="70" width="80" class="rounded zom" alt="no-image">
-                                    </span>
-                                </td>
+                                <th class="table-th text-white">CLIENTE</th>
+                                <th class="table-th text-center text-white">DIRECCION</th>
+                                <th class="table-th text-center text-white">TELEFONO</th>
+                                <th class="table-th text-center text-white">CORREO ELECTRONICO</th>
+                                <th class="table-th text-center text-white">SALDO</th>
 
                             </tr>
+                        </thead>
+                        <tbody>
 
+                            @foreach($deu as $c)
+                            <tr>
+                                <td>
+                                    <h6>{{$c->name}}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="text-center ">{{$c->address}}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="text-center">{{$c->phone}}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="text-center">{{$c->email}}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="text-center">$ {{$c->saldo}}</h6>
+                                </td>
+                            </tr>
                             @endforeach
                         </tbody>
+                        @elseif($estado==2)
+                        <thead class="text-white" style="background: #3B3F5C">
+                            <tr>
+                                <a class="btn btn-dark btn-block {{count($deu) <1 ? 'disabled' : '' }}" href="#" target="_black">GENERAR PDF DE MERIPUNTOS</a>
+
+                                <th class="table-th text-white">CLIENTE</th>
+                                <th class="table-th text-center text-white">DIRECCION</th>
+                                <th class="table-th text-center text-white">CORREO ELECTRONICO</th>
+                                <th class="table-th text-center text-white">MERIPUNTOS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach($deu as $c)
+                            <tr>
+                                <td>
+                                    <h6>{{$c->name}}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="text-center ">{{$c->address}}</h6>
+                                </td>
+
+                                <td>
+                                    <h6 class="text-center">{{$c->email}}</h6>
+                                </td>
+                                <td>
+                                    <h6 class="text-center">$ {{$c->meripuntos}}</h6>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        @elseif($estado==4)
+                        <thead class="text-white" style="background: #226F">
+                            <tr>
+                                <a class="btn btn-dark btn-block {{count($prod_exi) <1 ? 'disabled' : '' }}" href=" {{ url('produc_baj/pdf')}}" target="_black">GENERAR PDF DE PRODUCTOS EXISTENTES</a>
+                                <br>
+                                <th class="table-th text-white">NOMBRE</th>
+                                <th class="table-th text-white text-center">CODIGO BARRA</th>
+
+                                <th class="table-th text-white text-center">STOCK</th>
+
+                                <th class="table-th text-white text-center">CATEGORIA</th>
+                                <th class="table-th text-white text-center">IMAGEN</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(count($prod_exi) <1 ) <tr>
+                                <td class="text-center" colspan="5">
+                                    <h5>No hay resultados.</h5>
+                                </td>
+                                </tr>
+                                @endif
+
+                                @foreach ($prod_exi as $product)
+                                <tr>
+                                    <td>
+                                        <h6>{{$product->name}}</h6>
+                                    </td>
+                                    <td>
+                                        <h6 class="text-center">{{$product->barcode}}</h6>
+                                    </td>
+
+                                    <td>
+                                        <h6 class="text-center">{{$product->stock}}</h6>
+                                    </td>
+
+                                    <td>
+                                        <h6 class="text-center">{{$product->category}}</h6>
+                                    </td>
+
+                                    <td class="text-center">
+                                        <span>
+                                            <img src=" {{asset('storage/' . $product->imagen )}}" onclick="ShowImg('{{ asset('storage/' . $product->imagen) }}','{{$product->name}}')" height="70" width="80" class="rounded zom" alt="no-image">
+                                        </span>
+                                    </td>
+
+                                </tr>
+
+                                @endforeach
+                        </tbody>
+                        @endif
+
                     </table>
 
                 </div>
